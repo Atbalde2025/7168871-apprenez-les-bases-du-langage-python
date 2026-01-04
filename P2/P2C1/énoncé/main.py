f41316d85fae67926c6422ea30bf06e3f48220ee
Atbalde2025 <tanou772@gmail.com>
2026-01-04 17:09:33 +0000

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,9 @@
-# Ecrivez votre code ici !
+#les deux nombres
+nombre1=int(input("Donnez un premier nombre : "))
+nombre2=int(input("Donnez un deuxieme nombre : "))
+nombre1.isnumeric()
+nombre2.isnumeric()
+If nombre1.isnumeric() !=True and nombre2.isnumeric()!=True
+  raise SystemExit("Fin du programme")
+
+  
