64ba5b597909eaa9a96deab0c575714594ef91ca
Atbalde2025 <tanou772@gmail.com>
2026-01-04 15:34:39 +0000

    Update main.py
    Les variables et la determnation de type

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom= "Baldé"
+age = 24
+taille =1.68
+est_etudiant= False
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
